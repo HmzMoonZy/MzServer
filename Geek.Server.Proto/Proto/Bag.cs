18b44440122adae7c2f8a598764c60872d987e5b
hemengze <hemengze@dxx.cn>
2025-07-07 14:44:53 +0800

    - 移除一些不必要的逻辑

--- Geek.Server.Proto/Proto/Bag.cs
+++ Geek.Server.Proto/Proto/Bag.cs
@@ -21,32 +21,7 @@ namespace Geek.Server.Proto
     {
         public Dictionary<int, long> ItemDic { get; set; } = new Dictionary<int, long>();
     }
-
-    /// <summary>
-    /// 请求背包数据
-    /// </summary>
-    [MessagePackObject(true)]
-    public class ReqComposePet : Message
-    {
-        /// <summary>
-        /// 碎片id
-        /// </summary>
-        public int FragmentId { get; set; }
-    }
-
-    /// <summary>
-    /// 返回背包数据
-    /// </summary>
-    [MessagePackObject(true)]
-    public class ResComposePet : Message
-    {
-        /// <summary>
-        /// 合成宠物的Id
-        /// </summary>
-        public int PetId { get; set; }
-    }
-
-
+    
     /// <summary>
     /// 使用道具
     /// </summary>
